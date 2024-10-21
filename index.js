625f0072575f42807752e5fb82fbe583ba588186
BlazejMistak <blazej.mistak.bm@gmail.com>
2024-10-21 13:17:16 +0200

    navbar styling

---
+++ index.js
@@ -0,0 +1,8 @@
+function not_avaliable() {
+  alert("Function is currently unavaliable");
+}
+
+function test() {
+  const collapsible = document.querySelector(".collapsible");
+  collapsible.classList.toggle("collapsible-expanded");
+}
